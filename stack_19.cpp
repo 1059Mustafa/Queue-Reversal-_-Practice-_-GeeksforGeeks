13d027c88e3860fbab6056be93df9cdffb094b4f
1059Mustafa <72117500+1059Mustafa@users.noreply.github.com>
2021-02-14 11:42:54 +0530

    Add files via upload

--- stack_19.cpp
+++ stack_19.cpp
@@ -15,3 +15,23 @@ queue<int> rev(queue<int> q)
     }
     return q;
 }
+
+
+///  method 2 recursively
+
+void reverseQueue(queue<long long int>& q) 
+{ 
+    // Base case 
+    if (q.empty()) 
+        return; 
+  
+    // Dequeue current item (from front)   
+    long long int data = q.front(); 
+    q.pop(); 
+  
+    // Reverse remaining queue   
+    reverseQueue(q); 
+  
+    // Enqueue current item (to rear)   
+    q.push(data); 
+} 
